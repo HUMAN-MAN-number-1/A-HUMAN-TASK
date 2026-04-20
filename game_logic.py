55590c4f15577b1acf9522b17c441eda8385f0ee
PP_MASTER <liorhadar16@gmail.com>
2026-04-20 18:26:16 +0700

    ASDdsfghFdfasfd

--- game_logic.py
+++ game_logic.py
@@ -1,3 +1,6 @@
+from level import Level
+
+
 class Game:
     def __init__(self):
         self.friendly_units = dict()
@@ -6,6 +9,8 @@ class Game:
         self.friendly_units_on_screen = dict()
 
     def initialize(self):
+        level1 = Level(1, 'test')
+        level1.initialize()
         self.friendly_units_on_screen[1] = Wall(1, 'wall', 'glory hole', 5, [500, 600], 1, 1, 1)
         self.enemy_units_on_screen[1] = BasicEnemy(1, 'wall', 'glory hole', 5, 1, [100, 200], 1, 1,1)
 
